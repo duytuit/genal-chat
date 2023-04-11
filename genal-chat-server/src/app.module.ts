42e77e0bf755dbb76c6e8d9b4ceeed7910dba6e3
Duy Tu <duytu89@gmail.com>
2023-04-11 11:20:12 +0700

    dev

--- genal-chat-server/src/app.module.ts
+++ genal-chat-server/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import {TypeOrmModule, TypeOrmModuleOptions} from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 import { ChatModule } from './modules/chat/chat.module';
 import { FriendModule } from './modules/friend/friend.module';
@@ -7,18 +7,31 @@ import { GroupModule } from './modules/group/group.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TestModule } from './modules/test/test.module';
 
+ const _opntion_dev:TypeOrmModuleOptions = {
+  type: 'mysql',
+  port: 3306,
+  username: 'root',
+  password: 'Duytuit89!',
+  database: 'chat_socket',
+  charset: "utf8mb4", // 设置chatset编码为utf8mb4
+  autoLoadEntities: true,
+  synchronize: true
+};
+
+const _opntion_pro:TypeOrmModuleOptions = {
+  type: 'mysql',
+  port: 3306,
+  username: 'root',
+  password: 'Duytuit89!',
+  database: 'chat_socket_pro',
+  charset: "utf8mb4", // 设置chatset编码为utf8mb4
+  autoLoadEntities: true,
+  synchronize: true
+};
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'Duytuit89!',
-      database: 'chat_socket',
-      charset: "utf8mb4", // 设置chatset编码为utf8mb4
-      autoLoadEntities: true,
-      synchronize: true
-    }),
+    TypeOrmModule.forRoot(process.env.NODE_ENV == 'dev' ? _opntion_dev : _opntion_pro),
     UserModule,
     ChatModule,
     FriendModule,
